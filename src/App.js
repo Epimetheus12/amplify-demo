b67afc47ff3040ea3c447bd89cccdee7d866b5c7
Epimetheus12 <liangfangji@126.com>
2021-12-13 02:25:05 -0500

    test api with aws

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import logo from './logo.svg';
+import {useState, useEffect} from 'react'
+import axios from "axios";
 import './App.css';
 
+
 function App() {
+  const BASE_URL = "http://ebdemo-env.eba-4de9vv9b.us-east-2.elasticbeanstalk.com";
+  const [state, setState] = useState({username: '', createdTime: ''});
+  const [err, setErr] = useState({err: ''});
+
+  useEffect( () => {
+    const instance = axios.create({
+      baseURL: BASE_URL,
+      timeout: 1000
+    });
+    instance.get('/user/?id=1').then(
+        (data) => {
+          setState({username: data.username, createdTime: data.createdTime});
+          setErr({err:''});
+        }
+    ).catch(err => {
+      setErr({err: `${err.message}`})
+    })
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello tester!
+          Hello {state.username}!
+        </p>
+        <p>
+          Created: {state.createdTime}
+        </p>
+        <p hidden={err.err.length === 0}>
+          Error: {err.err}
         </p>
         <a
           className="App-link"
